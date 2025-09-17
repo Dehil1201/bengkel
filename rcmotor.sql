523670e576634ca6b57353476a5ba07990b2e679
Dehil <dede.hilman.1111@gmail.com>
2025-09-17 20:32:04 +0700

    add pembelian

--- rcmotor.sql
+++ rcmotor.sql
@@ -16,6 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `akun`
+--
+
+DROP TABLE IF EXISTS `akun`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `akun` (
+  `id_akun` int(11) NOT NULL AUTO_INCREMENT,
+  `kode_akun` varchar(20) NOT NULL,
+  `nama_akun` varchar(100) NOT NULL,
+  `jenis_akun` enum('Kas','Bank','Hutang','Piutang','Modal','Pendapatan','Beban') NOT NULL,
+  `keterangan` text DEFAULT NULL,
+  `id_bengkel` int(11) NOT NULL,
+  `tanggal_dibuat` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id_akun`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `akun`
+--
+
+LOCK TABLES `akun` WRITE;
+/*!40000 ALTER TABLE `akun` DISABLE KEYS */;
+INSERT INTO `akun` VALUES (1,'1001','BRI','Kas','Kas',4,'2025-09-17 11:12:07'),(2,'1002','Mandiri','Bank','Bank',4,'2025-09-17 11:12:40'),(3,'1003','BNI','Hutang','Hutang',4,'2025-09-17 11:17:27'),(4,'1004','BCA','Piutang','',4,'2025-09-17 11:18:14'),(5,'1005','SeaBank','Modal','tes',4,'2025-09-17 11:22:56');
+/*!40000 ALTER TABLE `akun` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `bengkels`
 --
 
@@ -67,7 +96,7 @@ CREATE TABLE `harga_jual_sparepart` (
   KEY `satuan_jual_id` (`satuan_jual_id`),
   CONSTRAINT `harga_jual_sparepart_ibfk_1` FOREIGN KEY (`sparepart_id`) REFERENCES `spareparts` (`id_sparepart`),
   CONSTRAINT `harga_jual_sparepart_ibfk_2` FOREIGN KEY (`satuan_jual_id`) REFERENCES `satuan` (`id_satuan`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,7 +105,7 @@ CREATE TABLE `harga_jual_sparepart` (
 
 LOCK TABLES `harga_jual_sparepart` WRITE;
 /*!40000 ALTER TABLE `harga_jual_sparepart` DISABLE KEYS */;
-INSERT INTO `harga_jual_sparepart` VALUES (1,1,1,0.00,130.00,1,1),(2,1,2,0.00,1300.00,2,0),(3,2,1,0.00,100.00,1,1),(4,3,1,0.00,2500.00,1,1),(5,3,2,0.00,25000.00,2,10);
+INSERT INTO `harga_jual_sparepart` VALUES (1,1,1,0.00,130.00,1,1),(2,1,2,0.00,1300.00,2,0),(3,2,1,0.00,100.00,1,1),(4,3,1,0.00,2500.00,1,1),(5,3,2,0.00,25000.00,2,10),(7,4,1,0.00,20000.00,1,1);
 /*!40000 ALTER TABLE `harga_jual_sparepart` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -308,7 +337,7 @@ CREATE TABLE `spareparts` (
   CONSTRAINT `spareparts_ibfk_2` FOREIGN KEY (`merk_id`) REFERENCES `merk_sparepart` (`id_merk`),
   CONSTRAINT `spareparts_ibfk_3` FOREIGN KEY (`satuan_beli_id`) REFERENCES `satuan` (`id_satuan`),
   CONSTRAINT `spareparts_ibfk_4` FOREIGN KEY (`bengkel_id`) REFERENCES `bengkels` (`id_bengkel`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -317,7 +346,7 @@ CREATE TABLE `spareparts` (
 
 LOCK TABLES `spareparts` WRITE;
 /*!40000 ALTER TABLE `spareparts` DISABLE KEYS */;
-INSERT INTO `spareparts` VALUES (1,'SP-1755528666','TEST',1,1,'',1000.00,2,10,100.00,65,10,4),(2,'4-SPART-816110258','kjdbfkjdnk',1,1,'',1000.00,2,100,10.00,80,10,4),(3,'4-SPART-357233986','jbvjsdbjsj',1,1,'',20000.00,2,10,2000.00,0,1,4);
+INSERT INTO `spareparts` VALUES (1,'SP-1755528666','TEST',1,1,'',1000.00,2,10,100.00,59,10,4),(2,'4-SPART-816110258','kjdbfkjdnk',1,1,'',1000.00,2,100,10.00,59,10,4),(3,'4-SPART-357233986','jbvjsdbjsj',1,1,'',20000.00,2,10,2000.00,17,1,4),(4,'4-SPART-741863857','dsjdncdjsk',1,1,'',10000.00,1,1,10000.00,10,5,4);
 /*!40000 ALTER TABLE `spareparts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -379,7 +408,7 @@ CREATE TABLE `stok_opnames` (
   KEY `spare_part_id` (`spare_part_id`),
   CONSTRAINT `stok_opnames_ibfk_2` FOREIGN KEY (`bengkel_id`) REFERENCES `bengkels` (`id_bengkel`) ON DELETE CASCADE,
   CONSTRAINT `stok_opnames_ibfk_3` FOREIGN KEY (`spare_part_id`) REFERENCES `spareparts` (`id_sparepart`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -388,7 +417,7 @@ CREATE TABLE `stok_opnames` (
 
 LOCK TABLES `stok_opnames` WRITE;
 /*!40000 ALTER TABLE `stok_opnames` DISABLE KEYS */;
-INSERT INTO `stok_opnames` VALUES (4,'2025-08-30',1,100,90,-10,'',4,'2025-08-30 05:31:56','2025-08-30 05:31:56'),(5,'2025-08-30',1,90,100,10,'',4,'2025-08-30 05:32:53','2025-08-30 05:32:53');
+INSERT INTO `stok_opnames` VALUES (4,'2025-08-30',1,100,90,-10,'',4,'2025-08-30 05:31:56','2025-08-30 05:31:56'),(5,'2025-08-30',1,90,100,10,'',4,'2025-08-30 05:32:53','2025-08-30 05:32:53'),(6,'2025-09-11',3,0,10,10,'',4,'2025-09-11 11:58:32','2025-09-11 11:58:32'),(7,'2025-09-11',2,80,80,0,'',4,'2025-09-11 11:58:32','2025-09-11 11:58:32'),(8,'2025-09-11',1,65,65,0,'',4,'2025-09-11 11:58:32','2025-09-11 11:58:32');
 /*!40000 ALTER TABLE `stok_opnames` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -494,7 +523,10 @@ CREATE TABLE `transaksi` (
   `no_faktur` varchar(50) NOT NULL,
   `tanggal` datetime NOT NULL DEFAULT current_timestamp(),
   `jenis` enum('penjualan','pembelian') NOT NULL,
+  `metode_bayar` varchar(50) NOT NULL,
   `total` decimal(15,2) NOT NULL DEFAULT 0.00,
+  `discount` decimal(50,2) NOT NULL,
+  `total_bayar` decimal(15,2) NOT NULL,
   `uang_bayar` decimal(15,2) NOT NULL,
   `kembalian` decimal(15,2) NOT NULL,
   `id_user` int(11) DEFAULT NULL,
@@ -513,7 +545,7 @@ CREATE TABLE `transaksi` (
   CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`) ON DELETE SET NULL,
   CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggans` (`id_pelanggan`) ON DELETE SET NULL,
   CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`id_bengkel`) REFERENCES `bengkels` (`id_bengkel`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -522,7 +554,7 @@ CREATE TABLE `transaksi` (
 
 LOCK TABLES `transaksi` WRITE;
 /*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
-INSERT INTO `transaksi` VALUES (1,'PJ.20250906.8.4.0001','2025-09-07 15:13:04','penjualan',40000.00,50000.00,10000.00,8,NULL,1,1,'Satria FU','Z01DF',4,'selesai'),(2,'PJ.20250906.8.4.0002','2025-09-06 16:39:09','penjualan',22200.00,0.00,0.00,8,NULL,NULL,NULL,'','',4,'pending'),(3,'PJ.20250906.8.4.0003','2025-09-06 16:40:00','penjualan',15650.00,0.00,0.00,8,NULL,NULL,NULL,'','',4,'pending'),(4,'PJ.20250906.8.4.0004','2025-09-06 16:40:42','penjualan',15650.00,0.00,0.00,8,NULL,NULL,NULL,'','',4,'pending'),(5,'PJ.20250906.8.4.0005','2025-09-06 16:42:41','penjualan',910.00,0.00,0.00,8,NULL,NULL,1,'','',4,'pending');
+INSERT INTO `transaksi` VALUES (1,'PJ.20250906.8.4.0001','2025-09-07 15:13:04','penjualan','',40000.00,0.00,0.00,50000.00,10000.00,8,NULL,1,1,'Satria FU','Z01DF',4,'selesai'),(2,'PJ.20250906.8.4.0002','2025-09-16 18:18:47','penjualan','Tunai',22200.00,10.00,0.00,0.00,0.00,8,NULL,NULL,NULL,NULL,NULL,4,'selesai'),(3,'PJ.20250906.8.4.0003','2025-09-16 18:37:29','penjualan','Tunai',15650.00,5.00,14867.00,20000.00,0.00,8,NULL,NULL,NULL,NULL,NULL,4,'pending'),(4,'PJ.20250906.8.4.0004','2025-09-16 18:01:59','penjualan','',15650.00,10.00,0.00,15000.00,915.00,8,NULL,NULL,NULL,NULL,NULL,4,'pending'),(5,'PJ.20250906.8.4.0005','2025-09-06 16:42:41','penjualan','',910.00,0.00,0.00,0.00,0.00,8,NULL,NULL,1,'','',4,'pending'),(6,'PJ.20250911.8.4.0001','2025-09-11 19:37:54','penjualan','',25000.00,0.00,0.00,50000.00,25000.00,8,NULL,1,NULL,NULL,NULL,4,'selesai'),(7,'PJ.20250915.8.4.0001','2025-09-15 22:39:40','penjualan','',25000.00,0.00,0.00,50000.00,25000.00,8,NULL,1,NULL,NULL,NULL,4,'selesai'),(8,'PS.20250915.8.4.0002','2025-09-15 22:40:40','penjualan','',40000.00,0.00,0.00,50000.00,10000.00,8,NULL,1,1,'jsfdsj','knsdfjs',4,'selesai'),(9,'PJ.20250916.8.4.0001','2025-09-16 17:45:04','penjualan','Tunai',1300.00,2.00,0.00,2000.00,726.00,8,NULL,1,NULL,NULL,NULL,4,'selesai'),(10,'PS.20250916.8.4.0002','2025-09-16 17:57:03','penjualan','Non Tunai',15130.00,10.00,0.00,15000.00,1.38,8,NULL,NULL,NULL,NULL,NULL,4,'pending'),(11,'PJ.20250916.8.4.0006','2025-09-16 18:39:13','penjualan','Non Tunai',2600.00,5.00,2470.00,10000.00,7.53,8,NULL,NULL,NULL,NULL,NULL,4,'selesai'),(12,'PB.20250917.8.4.0001','2025-09-17 20:18:02','pembelian','Tunai',10010.00,0.00,0.00,0.00,0.00,8,NULL,NULL,NULL,NULL,NULL,4,'selesai'),(13,'PB.20250917.8.4.0002','2025-09-17 20:19:45','pembelian','Tunai',20000.00,0.00,0.00,0.00,0.00,8,NULL,NULL,NULL,NULL,NULL,4,'selesai'),(14,'PB.20250917.8.4.0003','2025-09-17 20:20:53','pembelian','Tunai',2000.00,0.00,0.00,0.00,0.00,8,NULL,NULL,NULL,NULL,NULL,4,'selesai'),(15,'PB.20250917.8.4.0004','2025-09-17 20:23:27','pembelian','Tunai',30000.00,0.00,0.00,0.00,0.00,8,1,NULL,NULL,NULL,NULL,4,'selesai'),(16,'PB.20250917.8.4.0005','2025-09-17 20:28:54','pembelian','Tunai',18000.00,0.00,0.00,0.00,0.00,8,1,NULL,NULL,NULL,NULL,4,'selesai'),(17,'PB.20250917.8.4.0006','2025-09-17 20:30:23','pembelian','Tunai',16000.00,0.00,0.00,0.00,0.00,8,1,NULL,NULL,NULL,NULL,4,'selesai');
 /*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -540,7 +572,7 @@ CREATE TABLE `transaksi_detail_servis` (
   `nama_servis` varchar(100) NOT NULL,
   `biaya` decimal(15,2) NOT NULL,
   PRIMARY KEY (`id_detail`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -549,7 +581,7 @@ CREATE TABLE `transaksi_detail_servis` (
 
 LOCK TABLES `transaksi_detail_servis` WRITE;
 /*!40000 ALTER TABLE `transaksi_detail_servis` DISABLE KEYS */;
-INSERT INTO `transaksi_detail_servis` VALUES (7,'PJ.20250906.8.4.0001','1','Ganti Oli',15000.00),(8,'PJ.20250906.8.4.0002','1','Ganti Oli',15000.00),(9,'PJ.20250906.8.4.0003','1','Ganti Oli',15000.00),(10,'PJ.20250906.8.4.0004','1','Ganti Oli',15000.00),(11,'PJ.20250907.8.4.0001','1','Ganti Oli',15000.00);
+INSERT INTO `transaksi_detail_servis` VALUES (7,'PJ.20250906.8.4.0001','1','Ganti Oli',15000.00),(8,'PJ.20250906.8.4.0002','1','Ganti Oli',15000.00),(9,'PJ.20250906.8.4.0003','1','Ganti Oli',15000.00),(10,'PJ.20250906.8.4.0004','1','Ganti Oli',15000.00),(11,'PJ.20250907.8.4.0001','1','Ganti Oli',15000.00),(12,'PS.20250915.8.4.0002','1','Ganti Oli',15000.00),(13,'PS.20250916.8.4.0002','1','Ganti Oli',15000.00);
 /*!40000 ALTER TABLE `transaksi_detail_servis` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -568,9 +600,10 @@ CREATE TABLE `transaksi_detail_sparepart` (
   `harga` decimal(15,2) NOT NULL,
   `qty` int(11) NOT NULL,
   `satuan` varchar(20) NOT NULL,
+  `discount` decimal(15,2) NOT NULL,
   `subtotal` decimal(15,2) NOT NULL,
   PRIMARY KEY (`id_detail`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -579,7 +612,7 @@ CREATE TABLE `transaksi_detail_sparepart` (
 
 LOCK TABLES `transaksi_detail_sparepart` WRITE;
 /*!40000 ALTER TABLE `transaksi_detail_sparepart` DISABLE KEYS */;
-INSERT INTO `transaksi_detail_sparepart` VALUES (36,'PJ.20250906.8.4.0001','4-SPART-357233986','jbvjsdbjsj',2500.00,10,'PCS',25000.00),(37,'PJ.20250906.8.4.0002','4-SPART-816110258','kjdbfkjdnk',100.00,20,'PCS',2000.00),(38,'PJ.20250906.8.4.0002','SP-1755528666','TEST',1300.00,4,'KARTON',5200.00),(39,'PJ.20250906.8.4.0003','SP-1755528666','TEST',130.00,5,'PCS',650.00),(40,'PJ.20250906.8.4.0004','SP-1755528666','TEST',130.00,5,'PCS',650.00),(41,'PJ.20250906.8.4.0005','SP-1755528666','TEST',130.00,7,'PCS',910.00),(42,'PJ.20250907.8.4.0001','SP-1755528666','TEST',130.00,14,'PCS',1820.00);
+INSERT INTO `transaksi_detail_sparepart` VALUES (36,'PJ.20250906.8.4.0001','4-SPART-357233986','jbvjsdbjsj',2500.00,10,'PCS',0.00,25000.00),(37,'PJ.20250906.8.4.0002','4-SPART-816110258','kjdbfkjdnk',100.00,20,'PCS',0.00,2000.00),(38,'PJ.20250906.8.4.0002','SP-1755528666','TEST',1300.00,4,'KARTON',0.00,5200.00),(39,'PJ.20250906.8.4.0003','SP-1755528666','TEST',130.00,5,'PCS',0.00,650.00),(40,'PJ.20250906.8.4.0004','SP-1755528666','TEST',130.00,5,'PCS',0.00,650.00),(41,'PJ.20250906.8.4.0005','SP-1755528666','TEST',130.00,7,'PCS',0.00,910.00),(42,'PJ.20250907.8.4.0001','SP-1755528666','TEST',130.00,14,'PCS',0.00,1820.00),(43,'PS.20250911.8.4.0001','4-SPART-357233986','jbvjsdbjsj',2500.00,1,'PCS',0.00,2500.00),(44,'PJ.20250911.8.4.0001','4-SPART-357233986','jbvjsdbjsj',25000.00,1,'KARTON',0.00,25000.00),(45,'PJ.20250915.8.4.0001','4-SPART-357233986','jbvjsdbjsj',25000.00,1,'KARTON',0.00,25000.00),(46,'PS.20250915.8.4.0002','4-SPART-357233986','jbvjsdbjsj',25000.00,1,'KARTON',0.00,25000.00),(47,'PJ.20250915.8.4.0003','4-SPART-816110258','kjdbfkjdnk',100.00,21,'PCS',0.00,2100.00),(48,'PJ.20250915.8.4.0003','4-SPART-357233986','jbvjsdbjsj',2500.00,5,'PCS',0.00,125000.00),(49,'PS.20250915.8.4.0003','4-SPART-357233986','jbvjsdbjsj',2500.00,1,'PCS',0.00,2500.00),(50,'PJ.20250916.8.4.0001','SP-1755528666','TEST',1300.00,1,'KARTON',0.00,1300.00),(51,'PS.20250916.8.4.0002','SP-1755528666','TEST',130.00,1,'PCS',0.00,130.00),(52,'PJ.20250916.8.4.0006','SP-1755528666','TEST',1300.00,2,'KARTON',0.00,2600.00),(53,'PB.20250917.8.4.0001','4-SPART-357233986','jbvjsdbjsj',25000.00,10,'KARTON',0.00,0.00),(54,'PJ.20250917.8.4.0001','SP-1755528666','TEST',1300.00,2,'KARTON',0.00,2600.00),(55,'PB.20250917.8.4.0001','4-SPART-741863857','dsjdncdjsk',10000.00,1,'PCS',0.00,10000.00),(56,'PB.20250917.8.4.0001','4-SPART-816110258','kjdbfkjdnk',10.00,1,'PCS',0.00,10.00),(57,'PB.20250917.8.4.0002','4-SPART-741863857','dsjdncdjsk',10000.00,2,'PCS',0.00,20000.00),(58,'PB.20250917.8.4.0003','4-SPART-357233986','jbvjsdbjsj',2000.00,1,'KARTON',0.00,2000.00),(59,'PB.20250917.8.4.0004','4-SPART-741863857','dsjdncdjsk',10000.00,3,'PCS',0.00,30000.00),(60,'PB.20250917.8.4.0005','4-SPART-357233986','jbvjsdbjsj',2000.00,9,'KARTON',0.00,18000.00),(61,'PB.20250917.8.4.0006','4-SPART-357233986','jbvjsdbjsj',2000.00,8,'KARTON',0.00,16000.00);
 /*!40000 ALTER TABLE `transaksi_detail_sparepart` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -625,4 +658,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-09-11 18:36:37
+-- Dump completed on 2025-09-17 20:31:56
